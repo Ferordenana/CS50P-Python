54597678d0c1e7463bf5e4a04df7cac1b7510381
FerOrdenana <ef.ordenana@gmail.com>
2022-10-21 16:24:10 -0300

    deep.py

---
+++ deep.py
@@ -0,0 +1,14 @@
+universe_question = input("What's the answer to the great question?: ")
+
+universe_question = universe_question.lower().strip()
+if universe_question == "forty-two" or universe_question == "forty two":
+    print("Yes")
+elif universe_question == "42":
+    print("yes")
+else:
+    print("No")
+
+
+
+
+    
